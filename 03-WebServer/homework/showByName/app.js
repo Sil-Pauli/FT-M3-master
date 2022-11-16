d45654c966b59540af7d8f65f1a54908e39b8d2b
Sil-paulli <sil.pauli.93@gmai.com>
2022-11-16 11:02:14 -0300

    homework terminada

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,15 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+http.createServer (function(req, res){
+    console.log (`Llego un request: ${req.url}`);
+    fs.readFile (`./images${req.url}.jpg`, function(err,data){
+        if (err){
+            res.writeHead (404,{'Content-Type':'text/plain'});
+            http.request.end('Img not found');
+        } else {
+            res.writeHead (200,{'Content-Type':'image/jpeg'});
+            res.end (data);
+        }
+    });
+}) .listen(1337, '127.0.0.1');
